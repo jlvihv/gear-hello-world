2be2d364a96a5ca0d5469d192d3b4f90406e93bf
LouiseMedova <luisa.medova@gmail.com>
2022-12-15 14:55:13 +0300

    added debug

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,11 @@ unsafe extern "C" fn handle() {
     let input_message: InputMessages = msg::load().expect("Error in loading InputMessages");
     match input_message {
         InputMessages::SendHelloTo(account) => {
+            debug!("Message: SendHelloTo {:?}", account);
             msg::send(account, String::from("Hello"), 0).expect("Error in sending Hello message to account");
         },
         InputMessages::SendHelloReply => {
+            debug!("Message: SendHelloReply");
             msg::reply(String::from("Hello"), 0).expect("Error in sending reply");
         }
     }
@@ -28,6 +30,8 @@ unsafe extern "C" fn init() {
 
 gstd::metadata! {
     title: "Hello world contract",
+    init:
+        input: String,
     handle:
         input: InputMessages,
         output: String,
